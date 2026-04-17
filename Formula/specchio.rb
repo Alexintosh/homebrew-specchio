2b030040bb78c5c38cd37a7df23e2009aad72b36
Alessio Delmonti <alessio.d@gmail.com>
2026-04-17 17:49:57 +0100

    fix: add correct tarball checksum

--- Formula/specchio.rb
+++ Formula/specchio.rb
@@ -5,7 +5,7 @@ class Specchio < Formula
   desc "Interactive setup CLI for Specchio iOS development environment"
   homepage "https://github.com/Alexintosh/specchio-cli"
   url "https://github.com/Alexintosh/specchio-cli/archive/refs/tags/v1.0.4.tar.gz"
-  sha256 "DUMMY_SHA256"
+  sha256 "cc4b85010c252847ae3c8f4cd3fa84d1e24897e739208e1277d9cd5ab87c0e92"
   license "MIT"
 
   def install
